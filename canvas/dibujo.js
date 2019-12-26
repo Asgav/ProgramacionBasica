8be84d30b7ec91e641b4c4a1a3358da3eec0838f
Astrid G Acosta Vela <philonatura@gmail.com>
2019-12-25 22:17:42 -0500

    functions JS in Canvas

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,15 +1,41 @@
 alert("luna me quiere")
 var d = document.getElementById("dibujito");
-var lienzo = d.getContext("2d");
+if (d.getContext)
+{
+  var lienzo = d.getContext("2d");
+  lienzo.shadowOffsetX = 20;
+  lienzo.shadowOffsetY = 6;
+  lienzo.shadowBlur = 3;
+  lienzo.shadowColor = "purple";
+  lienzo.strokeStyle = "red"
+  lienzo.strokeRect(25, 25, 250, 200);
+  lienzo.strokeStyle = "darkblue"
+  lienzo.shadowColor = "blue";
+}
 console.log(lienzo)
 lienzo.beginPath();
-lienzo.strokeStyle = "darkred";
-lienzo.shadowOnsetX = 0;
-lienzo.shadowOffsetX = 300;
-lienzo.shadowOnsetY = 0;
-lienzo.shadowOffsetY = 300;
-lienzo.shadowColor = "rgba(78, 187, 255, 0.5)";
 lienzo.moveTo(100,100);
 lienzo.lineTo(200,100);
+lienzo.moveTo(200,100);
+lienzo.lineTo(200,200);
+lienzo.moveTo(100,100);
+lienzo.lineTo(100,200);
+lienzo.moveTo(200,200);
+lienzo.lineTo(100,200);
 lienzo.stroke();
 lienzo.closePath();
+
+function dibujarLinea(shadow_color, color, x_inicial, y_inicial, x_final, y_final)
+{
+  lienzo.beginPath();
+  lienzo.shadowColor = shadow_color;
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(x_inicial,y_inicial);
+  lienzo.lineTo(x_final,y_final);
+  lienzo.stroke();
+  lienzo.closePath()
+}
+dibujarLinea("red", "green", 400, 100, 500, 100);
+dibujarLinea("orange", "red", 400, 200, 500, 200);
+dibujarLinea("purple", "orange", 400, 100, 400, 200);
+dibujarLinea("blue", "darkblue", 500, 100, 500, 200);
